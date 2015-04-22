8735473be8f1de5d2d23dfabbfb0f76faeeb2e80
iwantwaffles <magyverwray@gmail.com>
2015-04-22 14:23:02 -0700

    1

--- Project/UnamiSushi/UnamiSushi/App_Start/BundleConfig.cs
+++ Project/UnamiSushi/UnamiSushi/App_Start/BundleConfig.cs
@@ -9,10 +9,12 @@ namespace UnamiSushi
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/jqBootstrapValidation.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -21,11 +23,17 @@ namespace UnamiSushi
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/agency.js",
+                      "~/Scripts/classie.js",
+                      "~/Scripts/cpbAnimatedHeader.js",
+                      "~/Scripts/contact_me.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/agency.css",
+                      "~/Content/font-awesome.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
