2660ce5838e694abfb685875b8938fd6e1110745
iwantwaffles <magyverwray@gmail.com>
2015-04-26 16:48:49 -0700

    partial view test

--- Project/UnamiSushi/UnamiSushi/Controllers/HomeController.cs
+++ Project/UnamiSushi/UnamiSushi/Controllers/HomeController.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UnamiSushi.DAL;
+using UnamiSushi.Models;
 
 namespace UnamiSushi.Controllers
 {
     public class HomeController : Controller
     {
+        private PrimaryContext db = new PrimaryContext();
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult Menu()
+        public ActionResult Partial()
         {
             return View();
         }
+        public ActionResult Menu()
+        {
+            return View(db.MenuCategories.ToList());
+        }
 
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
