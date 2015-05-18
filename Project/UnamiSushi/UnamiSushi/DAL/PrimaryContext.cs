3e988b1131a087e6e535875745caba0da2a46b5e
jpbertsch <jpbertsch91@gmail.com>
2015-05-18 12:47:55 -0700

    Added users and administrators to context
    Added users and administrators to the primary context

--- Project/UnamiSushi/UnamiSushi/DAL/PrimaryContext.cs
+++ Project/UnamiSushi/UnamiSushi/DAL/PrimaryContext.cs
@@ -18,7 +18,11 @@ namespace UnamiSushi.DAL
         public DbSet<Subcategory> Subcategories { get; set; }
         public DbSet<MenuItem> MenuItems { get; set; }
         public DbSet<MenuPicture> MenuPictures { get; set; }
+        
+        public DbSet<ApplicationUser> Users { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        
+        public DbSet<Administrator> Administrators { get; set; }
         public DbSet<Reply> Replies { get; set; }
 
         // Dbset for users?
