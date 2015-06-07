3740fdd861093b305f6a2e1a2cecb08f096b0ea2
iwantwaffles <magyverwray@gmail.com>
2015-06-07 13:22:00 -0700

    re-published

--- Project/UnamiSushi/UnamiSushi/Migrations/Configuration.cs
+++ Project/UnamiSushi/UnamiSushi/Migrations/Configuration.cs
@@ -11,7 +11,7 @@ namespace UnamiSushi.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(UnamiSushi.DAL.PrimaryContext context)
